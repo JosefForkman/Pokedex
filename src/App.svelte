cc7be45977a0badabc940711a2b027e056951a13
Josef Forkman <strand.vatten@outlook.com>
2023-05-17 14:52:56 +0200

    add tanstack

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+  const queryClient = new QueryClient()
 </script>
 
-<main>
-  <h1>Pokedex, woooo!!!!</h1>
-</main>
+<QueryClientProvider client={queryClient}>
+  <main>
+    <h1>Pokedex, woooo!!!!</h1>
+  </main>
+</QueryClientProvider>
 
 <style>
 </style>
